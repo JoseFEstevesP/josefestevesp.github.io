f5122f210c82fbe167b33cfb39708606a201c5db
Jose Esteven <josefelixestevenpacheco@gmail.com>
2022-03-09 22:43:19 -0400

    feat: :lipstick: modo oscuro
    modo oscuro agregado los botones y la funcionalidad
    
    se agrego los botones se estelirisaron y se agrego la funcinalidad, falta el boton del idioma

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 const DG = selector => document.getElementById(selector);
+// menu
 const menuBtn = DG('menuBtn');
 menuBtn.addEventListener('click', e => {
 	e.target.firstElementChild.classList.toggle('menu__barras--animation');
 	e.target.nextElementSibling.firstElementChild.classList.toggle(
 		'menu__contMenu--show'
 	);
+});
+// theme
+const btnTheme = DG('btnTheme');
+let theme = localStorage.getItem('theme');
+if (theme) document.documentElement.dataset.theme = theme;
+btnTheme.addEventListener('click', () => {
+	let curretTheme = document.documentElement.getAttribute('data-theme');
+	let targetTheme = 'light';
+	if (curretTheme === 'light') {
+		targetTheme = 'dark';
+	}
+	document.documentElement.dataset.theme = targetTheme;
+	localStorage.setItem('theme', targetTheme);
 });
