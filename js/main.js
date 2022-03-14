895978dec34d0860859bd434fc8c6f91c48c57b9
Jose Esteven <josefelixestevenpacheco@gmail.com>
2022-03-14 01:33:23 -0400

    feat: :art: idiama y thema arreglado
    se agrego las funcionalidad de cambio de inioma y se arreglo el peqño fallo del modo oscuro
    
    1) idioma agregado al menu, falta agregar mas contenido para agregarlo al cambio de dioma.2)se cambio el dark theme a uno un poco mas facil

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const DG = selector => document.getElementById(selector);
+const QAll = selector => document.querySelectorAll(selector);
 // menu
 const menuBtn = DG('menuBtn');
 menuBtn.addEventListener('click', e => {
@@ -9,27 +10,73 @@ menuBtn.addEventListener('click', e => {
 });
 // theme
 const btnTheme = DG('btnTheme');
-let theme = localStorage.getItem('theme');
-if (theme) document.documentElement.dataset.theme = theme;
-btnTheme.addEventListener('click', e => {
-	let curretTheme = document.documentElement.getAttribute('data-theme');
-	let targetTheme = 'light';
-	if (curretTheme === 'light') {
-		targetTheme = 'dark';
-	}
-	document.documentElement.dataset.theme = targetTheme;
-	localStorage.setItem('theme', targetTheme);
-	const iconThemeObject = {
-		dark: 'sun-solid',
-		light: 'moon-solid',
-	};
-	const iconTheme = iconThemeObject[targetTheme];
-	e.target.firstElementChild.lastElementChild.setAttribute(
-		'src',
-		`./assets/icons/${iconTheme}.svg`
-	);
-	e.target.firstElementChild.firstElementChild.setAttribute(
+const iconTheme = DG('iconTheme');
+const iconDefoult = DG('iconDefoult');
+const prefresDarkScheme = window.matchMedia(
+	'(prefers-color-scheme: dark)'
+).matches;
+const iconThemeOpction = {
+	dark: 'sun',
+	light: 'moon',
+};
+const icon = IT => {
+	iconDefoult.setAttribute(
 		'srcset',
-		`./assets/icons/${iconTheme}.svg`
+		`assets/icons/${iconThemeOpction[IT]}.svg`
 	);
+	iconTheme.setAttribute('src', `assets/icons/${iconThemeOpction[IT]}.svg`);
+};
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+	document.documentElement.classList.toggle('dark');
+	icon(currentTheme);
+} else if (currentTheme === 'light') {
+	document.documentElement.classList.toggle('light');
+	icon(currentTheme);
+}
+btnTheme.addEventListener('click', e => {
+	let theme;
+	if (prefresDarkScheme) {
+		document.documentElement.classList.toggle('light');
+		theme = document.documentElement.classList.contains('light')
+			? 'light'
+			: 'dark';
+		icon(theme);
+	} else {
+		document.documentElement.classList.toggle('dark');
+		theme = document.documentElement.classList.contains('dark')
+			? 'dark'
+			: 'light';
+		icon(theme);
+	}
+	localStorage.setItem('theme', theme);
 });
+// languge
+const language = DG('language');
+language.addEventListener('click', e => {
+	e.target.nextElementSibling.classList.toggle('contentLanguage--show');
+});
+const languageOption = DG('languageOption');
+const textsToChange = QAll('[data-section]');
+const changeLanguage = async language => {
+	const requestJson = await fetch(`./language/${language}.json`);
+	const texts = await requestJson.json();
+	for (const textToChange of textsToChange) {
+		const section = textToChange.dataset.section;
+		const value = textToChange.dataset.value;
+		textToChange.textContent = texts[section][value];
+	}
+};
+languageOption.addEventListener('click', e => {
+	changeLanguage(e.target.parentElement.dataset.language);
+	language.dataset.lang = e.target.parentElement.dataset.language;
+	localStorage.setItem('lang', e.target.parentElement.dataset.language);
+});
+console.log(localStorage.getItem('lang'));
+if (localStorage.getItem('lang')) {
+	changeLanguage(localStorage.getItem('lang'));
+	language.dataset.lang = localStorage.getItem('lang');
+} else {
+	changeLanguage(window.navigator.language);
+	language.dataset.lang = window.navigator.language;
+}
